de72cb595f16f33d003b300f1bc930cb23fe48b3
AndreaVirgilio <48619641+AndreaVirgilio@users.noreply.github.com>
2019-03-16 12:02:59 +0100

    Change text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ message: 'OK' });
+  res.json({ message: 'API OK' });
 });
 
 module.exports = router;
